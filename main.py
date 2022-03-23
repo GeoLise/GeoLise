7c6b3c60153487f85dc8f4c662cdd171499c94b0
GeoLise <101557691+GeoLise@users.noreply.github.com>
2022-03-23 09:28:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,5 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1=types.KeyboardButton("Кнопка")
-    item2=types.KeyboardButton("Кнопка2")
-    item3=types.KeyboardButton("Игра")
-    markup.add(item1, item2, item3)
-    bot.send_message(message.chat.id, text="Привет, {0.first_name}! Я тестовый бот".format(message.from_user), reply_markup=markup)
+    bot.send_message(message.chat.id, text="Привет")
     
