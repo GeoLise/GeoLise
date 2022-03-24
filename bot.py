6439a653e5d286fdf01a2ad8feb256b56cea763f
GeoLise <101557691+GeoLise@users.noreply.github.com>
2022-03-24 19:10:22 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,11 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-
+    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1=types.KeyboardButton("Кнопка")
+    item2=types.KeyboardButton("Кнопка2")
+    item3=types.KeyboardButton("Игра")
+    markup.add(item1, item2, item3)
     bot.send_message(message.chat.id, text="Привет")
     
 if __name__=='__main__':
