32f77af026f8420987136a43f818874fafa090b9
GeoLise <101557691+GeoLise@users.noreply.github.com>
2022-03-24 19:03:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
+    markup=types.KeyboarMarkup(resize_keyboard=True)
     bot.send_message(message.chat.id, text="Привет, {0.first_name}!".format(message.from_user), reply_markup=markup)
     
 if __name__=='__main__':
