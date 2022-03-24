681aabdd9d6d607047d01349c1fd2993b8133a96
GeoLise <101557691+GeoLise@users.noreply.github.com>
2022-03-24 19:29:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,27 @@
 import telebot
+from telebot import types
 token = '5230048546:AAHvCul2FoIWxU5lfGUT7zmVQPsiJ8iPdKA'
 bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
-def start_message(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("jfvbdjhb")
-    markup.add(btn1)
-    bot.send_message(message.chat.id, text="Привет")
-    
+def start(message):
+    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1=types.KeyboardButton("Кнопка")
+    item2=types.KeyboardButton("Кнопка2")
+    item3=types.KeyboardButton("Игра")
+    markup.add(item1, item2, item3)
+    bot.send_message(message.chat.id, text="Привет, {0.first_name}! Я тестовый бот.".format(message.from_user), reply_markup=markup)
 
-    
-if __name__=='__main__':
-    bot.infinity_polling()
+@bot.message_handler(content_types='text')
+def message_reply(message):
+    if message.text=="Кнопка":
+        bot.send_photo(message.chat.id, photo=open('vze2.jpg', 'rb'))
+    if message.text=="Кнопка2":
+        bot.send_message(message.chat.id, "vk.com/goshkazavr")
+
+@bot.message_handler(content_types='text')
+def game(message):
+    if message.text=="Игра":
+        bot.send_message(message.chat.id, "игра началась")
+if __name__ == '__main__':
+     bot.infinity_polling() 
