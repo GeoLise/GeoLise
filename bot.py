d3add5c0b2997551e772723d5f554313be9a905b
GeoLise <101557691+GeoLise@users.noreply.github.com>
2022-03-24 20:54:36 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,6 +18,7 @@ def message_reply(message):
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         item1 = types.KeyBoardButton("Gfh")
         markup.add(item1)
+        bot.send_message(message.chat.id, text"kdjvbd")
     if message.text=="Глава 2":
         bot.send_message(message.chat.id, "vk.com/goshkazavr")
 
