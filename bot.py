88d2de50166b88a1204eef25fa1102fbbaa105a7
GeoLise <101557691+GeoLise@users.noreply.github.com>
2022-03-24 18:59:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, text="Привет")
+    bot.send_message(message.chat.id, text="Привет, {0.first_name}!".format(message.from_user), reply_markup=markup)
     
 if __name__=='__main__':
     bot.infinity_polling()
