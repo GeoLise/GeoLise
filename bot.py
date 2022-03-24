0350e9df3c124d5a29383c3a4a5d21387f390c18
GeoLise <101557691+GeoLise@users.noreply.github.com>
2022-03-24 19:16:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,12 +4,12 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1=types.KeyboardButton("Кнопка")
-    item2=types.KeyboardButton("Кнопка2")
-    item3=types.KeyboardButton("Игра")
-    markup.add(item1, item2, item3)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("jfvbdjhb")
+    markup.add(btn1)
     bot.send_message(message.chat.id, text="Привет")
+    
+
     
 if __name__=='__main__':
     bot.infinity_polling()
